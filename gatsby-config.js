c87f67b28d2c9351e1c20664884c84914aeb2578
Marvin Arnold <marvin@geeky.rocks>
2018-09-11 08:52:20 -0500

    Added pgp key

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,6 +1,6 @@
 module.exports = {
   siteMetadata: {
-    title: 'Gatsby Default Starter',
+    title: "Marvin's site",
   },
   plugins: ['gatsby-plugin-react-helmet'],
 }
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,12 +1,46 @@
 import React from 'react'
-import Link from 'gatsby-link'
 
 const IndexPage = () => (
   <div>
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <Link to="/page-2/">Go to page 2</Link>
+  <h1>Contact me</h1>
+  <p>
+    <a href="mailto:marvin@geeky.rocks">marvin@geeky.rocks</a>
+  </p>
+
+  <h2>PGP</h2>
+  <code>
+  -----BEGIN PGP PUBLIC KEY BLOCK-----
+
+  mQENBFiyHEoBCADUDCBQrcOjvyYQUUVNth7ndgAhQ2u0eFjpwIzoxVC5sn6pDd9M
+  MurDOkDhgmKkVxg/0syt2LpQPQued3HOBOrcri6+B67IVhzW07XLf8oHTxXujwvv
+  iHqYzaRMHvMrS9CL9ce7I8l6OEJtNC/oCbPpug0WSRz1ODUBakFRZd8cyw/5htuQ
+  Y1Aa1Nndw0xbFdZyyo8Zm2KscgEoMPWZviCSUumir3NlPDTKKt6XfnpAivVUtOU9
+  LYDZAc58IOzDs4nWPxB5UZPxmrUiLypHVVm/WCNYfSOnm2kKhfYNdvHRoSY+mzyt
+  /zbSXl5mjyF6y2EtrCTHYC9zIMs5nOi5yg4vABEBAAG0Ik1hcnZpbiBBcm5vbGQg
+  PG1hcnZpbkBnZWVreS5yb2Nrcz6JATcEEwEIACEFAliyHEoCGwMFCwkIBwIGFQgJ
+  CgsCBBYCAwECHgECF4AACgkQaxHEOGcyAQJIiQf/VP1ICsr80Py/5iKSBw7iJrbX
+  iemIP62oz/sM1cwqTk8wUrCySh8FFUpjm/ivF54T9ptxFQmtVITnMgMzTHR9x2MV
+  rJ/MN1ST3mQiagsGGRkArX1IqO6zOVd67xTiuMq7g5rzqcVGBIG054xrMOjXANNy
+  7m637X0lOvsiWj7+ZrJ5a5sbf8BvkeRbbRGyiUvMd8xyXkKSx4f4iQB8daTcQPCU
+  cK9QZN6PxI2d+SkAW2L/1FlAoaC9i1BLQeLyOcKG5wsRQdIB3/CEXbaAULQcO+ju
+  Jj+dbR6rGtlbCqpLdxuCiHYBdgfelJTXHIB7MDeSM6WW0+r3kfrNyxHXqTLwV7kB
+  DQRYshxKAQgAr9Z5xyypoLbF+9/2XHaOZ+ZGKxNUjLQSSqhoQvZ3PFu9CrF2jiPo
+  VsUIBJOJXqgD1S7ziBBUenOfV2aLv5YbdSlbh7YJUKR95IihQukqNcVEigyxM/WF
+  ftln0pWzfGNwMf8KmGkLD8JTmoHFJ34AXSMDb+fM/RYFMlyy1HmUBgL2+rEkRTr8
+  Fm08hJ9XGT/ZCvQ6OoQm/Urr6LDFgwOGAndyuLP/O8WYcbk2ddnwLxKKQXMXBdoz
+  3DNeNGxRBBCiPn0rZU7zFvcDlOG3JHl8w7d0FIS7FqFY69QEsUrEDntc4h+AApwi
+  CUvJd440PJmquYGC4CUd9tWDz2M2wAqxNQARAQABiQEfBBgBCAAJBQJYshxKAhsM
+  AAoJEGsRxDhnMgECrn0H/ib7/qTSz4flyrTAsGlTBpXog63KW6JPO8FW8tkdkKas
+  g2uAmzRqMb+DznsC39D6p56Awp8wpzufew1UXwUZl3APIomUnf7yp15eUvo3QXYc
+  UOwkZ7UIlI2S/leFogPc2BpoTpQazwxN/tJypO5fFSUbnF4ODTOL/rRXW6efLRLE
+  yVsK3NGLGDOLATCnWwQlWQ6A29/YxUTePvB9S+fRiufCBmGnxTCqRsYRRYBLqlpU
+  3RuZUjo1rBAV8mE9f5TcL/9f9q+1lFHugoMg4ktjfmPdjfsrNZaztMp4PtfVIzdz
+  JItx0H+6lttSZc+2hipLjYtbd+DIYRwH107OrZjA9Xc=
+  =rdGP
+
+  -----END PGP PUBLIC KEY BLOCK-----
+
+  </code>
   </div>
 )
 
